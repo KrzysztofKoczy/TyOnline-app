c1e5bad23f52a48edfa960dc0fe3f1c35a110899
Krzysztof <krzysztof.koczy99@gmail.com>
2025-10-30 21:27:56 +0100

    add rederection to error in interceptor

--- src/app/core/interceptors/token.interceptor.ts
+++ src/app/core/interceptors/token.interceptor.ts
@@ -1,11 +1,14 @@
 import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
+import { Router } from '@angular/router';
 import { catchError, throwError } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 
 
 export const authTokenInterceptor: HttpInterceptorFn = (req, next) => {
   const auth = inject(AuthService);
+  const router = inject(Router);
+  
   const token = auth.tokenSignal();
   const withAuth = token
     ? req.clone({ setHeaders: { Authorization: `Bearer ${token}` } })
@@ -15,6 +18,11 @@ export const authTokenInterceptor: HttpInterceptorFn = (req, next) => {
     catchError((error) => {
       if (error instanceof HttpErrorResponse && error.status === 401) {
         auth.clearLocalSession();
+        try {
+          if (router.url !== '/login') {
+            void router.navigateByUrl('/login');
+          }
+        } catch {}
       }
       
       return throwError(() => error);
