3c774b7f75baef652f680c0110905e5365c1b238
Krzysztof <krzysztof.koczy99@gmail.com>
2025-10-30 16:46:08 +0100

    configure routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { authGuard, redirectLoggedInGuard } from './core/guards/auth.guard';
+import { MainLayout } from './core/layout/main-layout/main-layout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{ path: '', pathMatch: 'full', redirectTo: 'news' },
+	{
+		path: 'login',
+		canActivate: [redirectLoggedInGuard],
+		loadChildren: () => import('./features/login/routes').then(r => r.loginRoutes),
+	},
+	{
+		path: '',
+		component: MainLayout, 
+		canActivate: [authGuard],
+		children: [
+		  {
+			path: 'news',
+			loadChildren: () => import('./features/news/routes').then(r => r.newsRoutes),
+		  },
+		  {
+			path: '',
+			redirectTo: 'news',
+			pathMatch: 'full'
+		  }
+		]
+	  },
+	{ path: '**', redirectTo: 'login' },
+];
